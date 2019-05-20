241804bf7138efc245e39c927ce52244471f4720
vuyisile <vuyisilemark@gmail.com>
2019-05-20 12:31:00 +0200

    getting started with dependency injection and Guice

---
+++ guice-tutorial/src/main/java/BillingService.java
@@ -0,0 +1,15 @@
+public class BillingService {
+    private final CreditCardProcessor processor;
+    private final TransactionLog transactionLog;
+
+    @Inject
+    BillingService(CreditCardProcessor processor,
+                   TransactionLog transactionLog) {
+        this.processor = processor;
+        this.transactionLog = transactionLog;
+    }
+
+    public Receipt chargeOrder(PizzaOrder order, CreditCard creditCard) {
+
+    }
+}
