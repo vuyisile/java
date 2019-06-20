115875aa1e9bbfd79a1c70220d033a46b7ba4201
vuyisile <vuyisilemark@gmail.com>
2019-06-20 09:33:14 +0200

    initializing app

---
+++ storage-app/storage-app/src/test/java/com/skywalker/storageapp/storageapp/StorageAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skywalker.storageapp.storageapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StorageAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
