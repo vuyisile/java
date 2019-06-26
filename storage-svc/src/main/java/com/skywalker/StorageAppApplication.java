9d5e4485ffd525cc906fa9eb5f4429f4c7d7c2f9
vuyisile <vuyisilemark@gmail.com>
2019-06-26 14:20:04 +0200

    renamed storage-svc to storage-service-application & fixed tomcat error i had

---
+++ storage-svc/src/main/java/com/skywalker/StorageAppApplication.java
@@ -0,0 +1,14 @@
+package com.skywalker.storageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageAppApplication {
+  //  public static void main(String[] args) {
+    //    SpringApplication.run(StorageAppApplication.class, args);
+   // }
+    public static void main (String [] args){
+        SpringApplication.run(StorageAppApplication.class,args);
+    }
+}
--- storage-svc/src/main/java/com/skywalker/storageservice/StorageServiceApplication.java
+++
@@ -1,6 +0,0 @@
-package com.skywalker.storageservice;
-
-public class StorageServiceApplication {
-    public void main(String[] args){
-    }
-}
